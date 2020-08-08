1d4dd851fb6184c7cbdf28a475148f0fea568e83
Vedang Jotaniya <60702378+VedangJotaniya@users.noreply.github.com>
2020-08-08 13:26:51 +0530

    Add files via upload

---
+++ temp.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Mymain
+{
+    public static void Main()
+    {
+        int T, N, NR, NC;
+        T=Convert.TOInt32(Console.ReadLine());
+        int [,] result = new int [T, 3];
+
+        for(int i=1;i<=T;i++)
+        {
+            N=Convert.TOInt32(Console.ReadLine());
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
